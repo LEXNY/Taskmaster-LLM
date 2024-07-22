9780c3a61cb3cea335668beccb10c3bb42b045b3
Alex <lex@Absurd-Reckonings.local>
2024-07-22 11:14:36 -0700

    formatting the way I like

--- src/App.js
+++ src/App.js
@@ -10,28 +10,20 @@ const useLanguage = (engine) => {
 
     const query = async (content) => {
       try {
-        const request = {
-          stream: false, n: 1, max_tokens: 128,
+        await engine.chatCompletion({
+          response_format: { type: "json_object" }, stream: false,
           messages: [{ role: "user", content }],
-          response_format: { type: "json_object" },
-        }
-        await engine.chatCompletion(request)
+        })
         const message = await engine.getMessage()
-        console.log("MESSAGE: ", message) // TODO
-        setResponse(
-          message // TODO
-          //JSON.parse( // TODO
-          //await engine.getMessage()
-          //)
-        )
+        setResponse( /* TODO: JSON.parse( */ message /* ) */ )
       } catch (e) { setResponse(`ERROR: ${e}`) }
     }
 
     return { query, response }
   }
 
-const App = () => {
-  // component values must be wrapped because of lazy initialization.
+export default ()=>{
+  // Wrap setStage values in extra functions.  Google "useState component lazy init".
   const [CurrentStage, setStage] = useState(() => () => "Downloading acerbic wit...")
   const [scene, setScene] = useState({ description: '', characters: {} })
 
@@ -43,16 +35,12 @@ const App = () => {
       setEngine(await language.CreateMLCEngine("SmolLM-135M-Instruct-q4f32_1-MLC"))
     })()
   })
-  useEffect(() => {
-    if(engine) setStage(() => CharacterStage)
-  }, [engine])
+  useEffect(()=>{ if(engine) setStage(()=> CharacterStage) }, [engine])
 
   return <article>
-      <h1>Preposterous Gauntlet</h1>
-      <CurrentStage
-        scene={scene} setScene={setScene} setStage={setStage} query={query} response={response}
-      />
-    </article>
+    <h1>Preposterous Gauntlet</h1>
+    <CurrentStage
+      scene={scene} setScene={setScene} setStage={setStage} query={query} response={response}
+    />
+  </article>
 }
-
-export default App
