201ce5aed77834d32b2ec8c92794472fcab122a8
alex nye <nothankyou@spam.com>
2024-07-30 11:11:51 -0700

    complete character and challenge stages

--- src/stages/StrategyStage.jsx
+++ src/stages/StrategyStage.jsx
@@ -3,38 +3,56 @@ import SceneStage from './SceneStage'
 import { useSchematic } from '../hooks/useSchematic'
 
 
+const prompt = (character, challenge) => `
+  Come up with a strategy for your character for the challenge.
+
+  Character:
+  ===
+  ${character.description}
+  ===
+
+  Challenge:
+  ===
+  ${challenge}
+  ===
+`
+
 const schematic = {
   strategy: '[Tactics, Doctrine, Goals, Schemes].',
 }
 
-export default ({ gameState, query, setScene }) => {
-  const {strategy} = useSchematic(schematic)
+export default ({ gameState, setGameState, query, setScene }) => {
+  const { strategy } = useSchematic(schematic)
+  const [numBots] = 
 
   useEffect(() => {
+    // TODO: not preventing resetting strategy on same char.
     const needsStrategy = Object.entries(gameState.antagonists)
-      .find(a => !a.strategy)
+      .find((_, a) => !a.strategy)
     if (needsStrategy) {
+      const [name, antagonist] = needsStrategy
       query(
-        prompt, schematic,
-        ({ strategy }) => draft => { needsStrategy.strategy = strategy }
+        prompt(antagonist, gameState.challenge),
+        schematic,
+        ({ strategy }) => draft => {
+          draft.antagonists[name].strategy = strategy
+        }
       )
     }
-  }, [gameState.antagonists])
+  })
 
   return <div>
-    <p>
-      Come up with a strategy for your character for the challenge:
-    </p>
+    <p>{prompt(gameState.protagonist)}</p>
     {gameState.challenge.split("\n").map((r, i) => <p key={i}>{r}</p>)}
     <input key="strategy" {...strategy} />
     <button onClick={() => {
-      // TODO: Cannot add property strategy, object is not extensible.
-      // Immerjs related.  Need to use `setGameState`
-      gameState.protagonist.strategy = strategy.value
+      setGameState(draft => {
+        draft.protagonist.strategy = strategy.value
+      })
       setScene(SceneStage)
     }}>Submit</button>
     {Object.entries(gameState.antagonists).map(([name, { strategy }], i) => <p key={name}>
-      {name}: {strategy}
+      {name}: {JSON.stringify(strategy)}
     </p>)}
   </div>
 }
