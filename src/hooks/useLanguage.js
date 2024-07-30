cfaae2ec46ec0e82b671ebf90af0a844c971da79
alex nye <nothankyou@spam.com>
2024-07-30 10:22:24 -0700

    complete character and challenge stages

--- src/hooks/useLanguage.js
+++ src/hooks/useLanguage.js
@@ -1,44 +1,50 @@
-import { useEffect, useState } from "react"
-import * as language from '@mlc-ai/web-llm'
+import { useState, useEffect, useRef } from 'react';
+import * as language from '@mlc-ai/web-llm';
 
-
-// TODO: debounce because multiple requests interleave.
 export const useLanguage = (setter) => {
-  const [engine, setEngine] = useState(undefined)
+  const [engine, setEngine] = useState(undefined);
+  const [debounce, setDebounce] = useState(false);
+
   useEffect(() => {
     (async () => {
-      setEngine(await language.CreateMLCEngine("Phi-3-mini-4k-instruct-q4f16_1-MLC"))
-    })()
-  }, [])
+      setEngine(await language.CreateMLCEngine("Phi-3-mini-4k-instruct-q4f16_1-MLC"));
+    })();
+  }, []);
 
   if (engine) {
     return async (prompt, schematic, callback) => {
-      const strungifiedSchematic = JSON.stringify(schematic)
+      if(debounce) {return}
+      setDebounce(true);
+      const strungifiedSchematic = JSON.stringify(schematic);
+
       while (true) {
         try {
           await engine.chatCompletion({
-            stream: false, response_format: { type: "json_object" },
+            stream: false,
+            response_format: { type: "json_object" },
             temperature: 0.75,
             messages: [
               { role: "system", content: "You respond only with JSON." },
-              { role: "user", content: `repeat ${strungifiedSchematic}`},
+              { role: "user", content: `repeat ${strungifiedSchematic}` },
               { role: "assistant", content: strungifiedSchematic },
               { role: "user", content: 'Now use the same property keys, but fill in the values.' + prompt }
             ],
-          })
-          const message = await engine.getMessage()
+          });
+
+          const message = await engine.getMessage();
 
           // callback is the API-consumer-provided plan.
           // setter is the `useImmer` setter.
           // JSON.parse(message) is the generated content.
-          console.log(`generated: ${message}`)
-          const parsedResponse = JSON.parse(message)
-          setter(callback(parsedResponse))
-          break
+          console.log(`generated: ${message}`);
+          const parsedResponse = JSON.parse(message);
+          setter(callback(parsedResponse));
+          setDebounce(false);
+          break;
         } catch (e) {
-          console.error(e)
+          console.error(e);
         }
       }
-    }
+    };
   }
-}
+};
