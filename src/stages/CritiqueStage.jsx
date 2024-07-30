ce95cdded9ea9d43a37314bdd68f37c01de31086
alex nye <nothankyou@spam.com>
2024-07-30 10:46:21 -0700

    complete character and challenge stages

--- src/stages/CritiqueStage.jsx
+++ src/stages/CritiqueStage.jsx
@@ -1,7 +1,31 @@
 import React, { useEffect, useState } from 'react'
 
 
-export default ({ characters, challenge, query, response }) => {
+const prompt = 'TODO' /*`
+  You are the capricious judge of a comedy game show challenge:
+  ===
+  ${challenge}
+  ===
+          
+  Given the scene for ${name}:
+  ===
+  ${scene}
+  ===
+          
+  Write a critique of the character's performance.
+`*/
+
+const schematic = {
+  "opening volley": "[Opening volley of insults lambasting the character's performance and general life choices with theatrical disdain.]",
+  "grudging acknowledgement": "[Grudging acknowledgement of one positive moment or trait, undermined by a backhanded followup.]",
+  "explanation": "[Explanation for the score: - Adherence to the rules - Clever lateral thinking - Humorous moments - Overall effectiveness]",
+  "parting shot": "[A parting shot insult issuing final judgement on both the performance and the character.]",
+  "score": "[Score between 1-5, preferably < 4]",
+}
+
+
+export default ({ }) => {
+  /* TODO
      const states = {}
       Object.keys(characters).forEach(name => {
         states[name] = useState('')
@@ -11,33 +35,9 @@ export default ({ characters, challenge, query, response }) => {
           (async () => {
             const setState = states[name][1]
             // TODO: doesn't return anything.
-            const critique = await query(`
-              You are the capricious judge of a comedy game show challenge:
-              ===
-              ${challenge}
-              ===
-          
-              Given the scene for ${name}:
-              ===
-              ${scene}
-              ===
-          
-              Use this template to structure your critique:
-              ===
-              [Opening volley of insults lambasting the character's performance and general life choices with theatrical disdain.]
-          
-              [Grudging acknowledgement of one positive moment or trait, undermined by a backhanded followup.]
-          
-              [Explanation for the score:
-              - Adherence to the rules
-              - Clever lateral thinking
-              - Humorous moments
-              - Overall effectiveness]
-          
-              [A parting shot insult issuing final judgement on both the performance and the character.]
-              ===
-            `)
+            const critique = await query(prompt)
             setState(critique)
           })()
         }})
+    */
   }
--- src/stages/StrategyStage.jsx
+++ src/stages/StrategyStage.jsx
@@ -1,27 +1,40 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 import SceneStage from './SceneStage'
 import { useSchematic } from '../hooks/useSchematic'
 
 
 const schematic = {
-  input: '[Strategy Name]:  [Tactics or Actions].',
+  strategy: '[Tactics, Doctrine, Goals, Schemes].',
 }
 
-export default ({ characters, setCharacters, setScene, response }) => {
-  const userCharacter = Object.entries(characters)[0]
+export default ({ gameState, query, setScene }) => {
   const {strategy} = useSchematic(schematic)
-  // TODO: multiplex
+
+  useEffect(() => {
+    const needsStrategy = Object.entries(gameState.antagonists)
+      .find(a => !a.strategy)
+    if (needsStrategy) {
+      query(
+        prompt, schematic,
+        ({ strategy }) => draft => { needsStrategy.strategy = strategy }
+      )
+    }
+  }, [gameState.antagonists])
 
   return <div>
     <p>
       Come up with a strategy for your character for the challenge:
     </p>
-    {response.map((r, i) => <p key={i}>{r}</p>)}
+    {gameState.challenge.split("\n").map((r, i) => <p key={i}>{r}</p>)}
     <input key="strategy" {...strategy} />
     <button onClick={() => {
-      // TODO: multiplexing has to happen in the onClick also.
-      //deepSetCharacter(characters, setCharacters, userCharacter.name, { strategy: strategy.value })
+      // TODO: Cannot add property strategy, object is not extensible.
+      // Immerjs related.  Need to use `setGameState`
+      gameState.protagonist.strategy = strategy.value
       setScene(SceneStage)
     }}>Submit</button>
+    {Object.entries(gameState.antagonists).map(([name, { strategy }], i) => <p key={name}>
+      {name}: {strategy}
+    </p>)}
   </div>
 }
